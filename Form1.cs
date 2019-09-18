7df6e363ac34b42c18de070efe3521a8c3d5b6da
jshahriyarbadalov <shah.badalov91@gmail.com>
2019-09-18 23:52:20 +0400

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using ResimGallery.ORM.Entity;
+using ResimGallery.ORM.Facade;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ResimGallery
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = Products.LisTable();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void resimEkleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow == null)
+            {
+                return;
+            }
+
+            int id = (int)dataGridView1.CurrentRow.Cells["ProductId"].Value;
+
+            openFileDialog1.Title = "Resim Sec";
+            openFileDialog1.Filter = "Jpg |*.jpg| Png|*.png|";
+            DialogResult result=openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                FileStream fs=new FileStream(openFileDialog1.FileName, FileMode.Open,FileAccess.Read);
+
+                BinaryReader br=new BinaryReader(fs);
+                byte[] resim = br.ReadBytes((int)fs.Length);
+                br.Close();
+                fs.Close();
+
+                ProductPicture pp=new ProductPicture();
+                pp.ProductID = id;
+                pp.Picture = resim;
+                if (ProductsPicture.Add(pp))
+                {
+                    dataGridView1.DataSource = Products.LisTable();
+                    MessageBox.Show("Product added photo successful!");
+                }
+                else
+                {
+                    MessageBox.Show("Product  added photo not successful!");
+                }
+            }
+        }
+
+        private void resimGosterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow == null)
+            {
+                return;
+            }
+
+            int id = (int)dataGridView1.CurrentRow.Cells["ProductId"].Value;
+
+            ResimlerForm rf=new ResimlerForm();
+            rf.ProductID = id;
+            rf.ShowDialog();
+        }
+    }
+}
---
+++ ResimlerForm.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ResimGallery.ORM.Facade;
+
+namespace ResimGallery
+{
+    public partial class ResimlerForm : Form
+    {
+        public ResimlerForm()
+        {
+            InitializeComponent();
+        }
+
+        public int ProductID { get; set; }
+
+        DataTable ResimDoldur()
+        {
+            DataTable dt = ProductsPicture.LisDataTable(ProductID);
+            return dt;
+        }
+
+        private void ResimlerForm_Load(object sender, EventArgs e)
+        {
+            DataTable dt = ResimDoldur();
+
+            foreach (DataRow item in dt.Rows)
+            {
+                ResimAta(item);
+                return;
+            }
+        }
+
+        private void ResimAta(DataRow item)
+        {
+            byte[] resim = (byte[]) item.ItemArray[2];
+            MemoryStream ms = new MemoryStream(resim.Length);
+            ms.Write(resim, 0, resim.Length);
+
+            Image img = Image.FromStream(ms);
+            pictureBox1.BackgroundImage = img;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+
+            pictureBox1.Tag = item.ItemArray[0];
+            return;
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            int id = (int) pictureBox1.Tag;
+
+            DataTable dt = ResimDoldur();
+            id++;
+            foreach (DataRow item in dt.Rows)
+            {
+                if ((int)item.ItemArray[0] == id)
+                {
+                    ResimAta(item);
+                }
+
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            int id = (int) pictureBox1.Tag;
+
+            DataTable dt = ResimDoldur();
+            id--;
+            foreach (DataRow item in dt.Rows)
+            {
+                if ((int)item.ItemArray[0] == id)
+                {
+                    ResimAta(item);
+                }
+
+            }
+        }
+    }
+}
